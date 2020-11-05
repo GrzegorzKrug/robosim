c5d8772f07b14e2e707998730b16a049999947aa
Greg <grzesiekk321@gmail.com>
2020-11-05 18:05:43 +0100

    Ploting pendulum in open cv for animation

--- modules/simplependulum.py
+++ modules/simplependulum.py
@@ -12,7 +12,7 @@ GRAV = 9.81
 
 
 class Model:
-    def __init__(self, cx, cy, mass=1, rope=4, initial_theta=30, friction=0.5, stepsize=1.0):
+    def __init__(self, cx, cy, mass=1, rope=5, initial_theta=30, friction=0.5, stepsize=1.0):
         """"""
         "Params"
         self.anchor = cx, cy
@@ -32,14 +32,13 @@ class Model:
                 "omega": [],
                 "x": [],
                 "y": [],
-                "tau": [],
         }
         "Initialization"
         self.x, self.y = self.position_on_arc(initial_theta)
 
     def position_on_arc(self, theta_deg):
         x, y = self.anchor
-        theta_rad = theta_deg * math.pi / 180
+        theta_rad = math.radians(theta_deg)
         x = x + self.rope * np.sin(theta_rad)
         y = y - self.rope * np.cos(theta_rad)
         return x, y
@@ -57,7 +56,7 @@ class Model:
 
         sn = math.sin(math.radians(theta_old))
         omega = omega_old * (1 - self.friction) - GRAV / self.rope * sn * step_size
-        theta = (theta_old + omega_old * step_size)  # % 360
+        theta = (theta_old + omega * step_size)  # % 360
 
         self.omega = omega
         self.theta = theta
@@ -70,21 +69,43 @@ class Model:
 
 
 center = (0, 0)
-model = Model(*center, initial_theta=90, stepsize=0.05, friction=0.001)
+model = Model(*center, initial_theta=190, stepsize=0.2, friction=0.001)
 initial = model.x, model.y
 all_trace = []
+offset_x = 300
+offset_y = 300
 
-for ti in range(5_000):
+for ti in range(1000):
+    array = np.zeros((600, 600, 3), dtype=np.uint8) + 170
     step = model.step()
     all_trace.append(step)
+    trace = list([*zip(*all_trace)])
+    poly = np.array(all_trace[-30:])
+    poly[:, 1] *= -1
+    poly[:, :] *= 50
+    poly[:, :] += 300
+    # poly = poly.reshape((-1, 1, 2))
+    # poly = np.array(poly, dtype=np.int32)
     x, y = step
-trace = list([*zip(*all_trace)])
+    x = int(x * 50) + 300
+    y = int(-y * 50) + 300
 
-fig = plt.figure()
-plt.plot(*trace)
-plt.scatter(*center, c='k', marker='.')
-plt.scatter(*initial)
-plt.axis('equal')
+    cv2.line(array, (offset_x, offset_y), (x, y), (0, 0, 0), 2)  # Line
+    cv2.circle(array, (offset_x, offset_y), 10, (0, 0, 160), -1)  # Anchor
+    for n, tr in enumerate(poly):
+        tr = np.array(tr, dtype=np.int32)
+        col = np.array([50, 200, 0]) * ( n / 30)
+        cv2.circle(array, tuple(tr), 8, col, -1)
+    # cv2.polylines(array, [poly], False, (180, 0, 0), 5)  # Trail
+    cv2.circle(array, (x, y), 10, (150, 200, 0), -1)  # Ball
+
+    cv2.imshow("Pendulum", array)
+
+    key = cv2.waitKey(10)
+    if key == ord("q"):
+        break
+
+trace = list([*zip(*all_trace)])
 
 fig2 = plt.figure()
 plt.plot(model.data['x'], label="x")
@@ -98,9 +119,5 @@ plt.legend()
 plt.subplot(212)
 plt.plot(model.data['omega'], label="omega")
 plt.legend()
-
-# fig4 = plt.figure()
-# plt.plot(model.data['tau'], label="tau")
-# plt.legend()
 
 plt.show()
