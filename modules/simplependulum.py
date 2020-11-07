2d52b656791973d7a9af2e529d9328b65b4e8584
Greg <grzesiekk321@gmail.com>
2020-11-08 00:48:41 +0100

    Pendulum in radians, Kinetic energy is delayed

--- modules/simplependulum.py
+++ modules/simplependulum.py
@@ -12,32 +12,41 @@ GRAV = 9.81
 
 
 class Model:
-    def __init__(self, cx, cy, mass=1, rope=5, initial_theta=30, initial_omega=0, friction=0.5, stepsize=1.0):
+    def __init__(self, cx, cy, mass=1, rope=5, initial_theta=1.0, initial_omega=0, friction=0.5, stepsize=1.0):
         """"""
         "Params"
         self.anchor = cx, cy
         self.mass = mass
         self.rope = rope
         self.friction = friction
+
         "Starting params"
         self.theta = initial_theta
+        self.initial_theta = initial_theta
         self.omega = initial_omega
         self.time = 0
         self.time0 = 0
         "Sim params"
+
         self.stepsize = stepsize
         "Collect Data"
         self.data = {
                 "theta": [],
+                "theta_deg": [],
                 "omega": [],
+                "veloc": [],
+                "lin_ve": [],
+                "radial": [],
+                "radial_deg": [],
                 "x": [],
                 "y": [],
                 "ep": [],
                 "ek": [],
                 "total_en": [],
+                "factor": [],
         }
         "Initialization"
-        self.x, self.y = self.position_on_arc(initial_theta)
+        self.x, self.y = self.position_on_arc(math.degrees(initial_theta))
 
     def position_on_arc(self, theta_deg):
         x, y = self.anchor
@@ -50,53 +59,64 @@ class Model:
         if not step_size:
             step_size = self.stepsize
         self.time += step_size
-        _TAU = round(self.time - self.time0, 4)
 
-        # self.theta = self.deritive(self.theta)
-        # return self.x, self.y
         theta_old = self.theta
         omega_old = self.omega
 
-        sinn = math.sin(math.radians(theta_old))
-        omega = omega_old * (1 - self.friction) - GRAV / self.rope * sinn * step_size
+        omega = omega_old * (1 - self.friction) - GRAV / self.rope * math.sin(theta_old) * step_size
         theta = (theta_old + omega * step_size)  # % 360
 
         self.omega = omega
         self.theta = theta
 
-        x, y = self.position_on_arc(theta)
-        self.data['theta'].append(theta)
+        theta_deg = math.degrees(theta)
+        x, y = self.position_on_arc(theta_deg)
+
+        self.data['theta_deg'].append(theta_deg)
         self.data['omega'].append(omega)
+
         self.data['x'].append(x)
         self.data['y'].append(y)
 
-        lin_distance = omega / 360 * self.rope
-        lin_ve = lin_distance * 2 * math.pi * self.rope
-        # lin_ve = omega * self.rope * step_size
+        velocity = math.sqrt(
+                2 * GRAV * self.rope * abs(math.cos(theta) - math.cos(self.initial_theta))
+        )
+
+        # print(omega)
+        lin_ve = (theta - theta_old) / step_size * self.rope
+        # lin_ve = math.radians(omega) * self.rope / step_size
+
+        # lin_ve = math.radians(omega / step_size) * self.rope
+        # lin_ve = math.radians(theta_old - theta) * self.rope / step_size
         # omg_rad = math.radians(omega)
-        # lin_ve = omg_rad * self.rope
+        # lin_ve = velocity
 
         # lin_ve = omega / 180 * math.pi * self.rope * self.rope
-        # lin_ve = omega * self.rope
+        # lin_ve = omega * self.rope  / 360
 
-        ep = self.mass * (self.rope + y) * GRAV
+        ep = self.mass * GRAV * (self.rope + y)
         ek = (1 / 2) * self.mass * (lin_ve ** 2)
         total_en = ep + ek
+
+        self.data['veloc'].append(velocity)
+        self.data['lin_ve'].append(lin_ve)
         self.data['ep'].append(ep)
         self.data['ek'].append(ek)
         self.data['total_en'].append(total_en)
+        self.data['factor'].append(velocity / omega)
 
         return x, y
 
 
+"Model Creation"
 center = (0, 0)
-model = Model(*center, mass=1, rope=5, initial_theta=170, initial_omega=0, stepsize=0.1, friction=0)
+model = Model(*center, mass=1, rope=5, initial_theta=math.radians(90), initial_omega=0, stepsize=.01, friction=0)
 initial = model.x, model.y
 all_trace = []
 offset_x = 300
 offset_y = 300
 
-for ti in range(750):
+for ti in range(1000):
     array = np.zeros((600, 600, 3), dtype=np.uint8) + 170
     step = model.step()
     all_trace.append(step)
@@ -105,8 +125,6 @@ for ti in range(750):
     poly[:, 1] *= -1
     poly[:, :] *= 50
     poly[:, :] += 300
-    # poly = poly.reshape((-1, 1, 2))
-    # poly = np.array(poly, dtype=np.int32)
     x, y = step
     x = int(x * 50) + 300
     y = int(-y * 50) + 300
@@ -117,17 +135,13 @@ for ti in range(750):
         tr = np.array(tr, dtype=np.int32)
         col = np.array([50, 200, 0]) * (n / 30)
         cv2.circle(array, tuple(tr), 8, col, -1)
-    # cv2.polylines(array, [poly], False, (180, 0, 0), 5)  # Trail
-    cv2.circle(array, (x, y), 10, (150, 200, 0), -1)  # Ball
+    cv2.circle(array, (x, y), 10, (250, 100, 50), -1)  # Ball
 
     cv2.imshow("Pendulum", array)
 
     key = cv2.waitKey(5)
     if key == ord("q"):
         break
-
-    # if model.theta < -30:
-    #     break
 
 trace = list([*zip(*all_trace)])
 
@@ -139,17 +153,25 @@ plt.plot(model.data['ep'], label="potential")
 plt.legend(loc=1)
 
 fig2 = plt.figure()
-plt.subplot(311)
+ax1 = plt.axes([0.1, 0.05, 0.8, 0.6])
+ax2 = plt.axes([0.1, 0.7, 0.8, 0.2])
+
+ax1.plot(model.data['veloc'], label="velocity (from mgh)", c='r')
+ax1.plot(model.data['lin_ve'], label="lin_ve", c=(1, 1, 0.1), linewidth=3)
+ax1.plot(model.data['omega'], label="omega", dashes=[10, 5], linewidth=3)
+# ax1.plot(model.data['radial'], label="radial")
+# ax1.plot(model.data['radial_deg'], label="radial_deg")
+# ax1.plot(model.data['factor'], label="factor")
+plt.plot()
+ax1.legend()
+
+ax2.plot(model.data['theta_deg'], label="theta degrees")
+ax2.legend()
+
+plt.figure()
+plt.subplot(312)
 plt.plot(model.data['x'], label="x")
 plt.plot(model.data['y'], label='y')
-plt.legend()
-
-plt.subplot(312)
-plt.plot(model.data['theta'], label="theta")
-plt.legend()
-
-plt.subplot(313)
-plt.plot(model.data['omega'], label="omega")
 plt.legend()
 
 plt.show()
