a1568c2f599fa5a823a73f4bfd0a407fc070c170
Greg <grzesiekk321@gmail.com>
2020-11-17 13:24:08 +0100

    Pendulum

--- modules/simplependulum.py
+++ modules/simplependulum.py
@@ -64,7 +64,8 @@ class SinglePendulum:
         theta_old = self.theta
         omega_old = self.omega
 
-        omega = omega_old * (1 - self.friction) - GRAV / self.rope * math.sin(theta_old) * step_size
+        accel = -GRAV * math.sin(theta_old) / self.rope
+        omega = omega_old * (1 - self.friction * step_size) + accel * step_size
         theta = theta_old + omega * step_size
 
         self.omega = omega
@@ -90,6 +91,7 @@ class SinglePendulum:
         )
 
         lin_ve = omega * self.rope
+
         h = self.rope * (1 - math.cos(theta))
 
         ep = self.mass * GRAV * h
@@ -104,7 +106,6 @@ class SinglePendulum:
         self.data['h'].append(h)
 
         return x, y
-
 
 
 def val_matrix_to_color_matrix(arr):
@@ -132,7 +133,7 @@ def pendulum_gradient(plot_range=5, N=45, rope=3, step_size=0.001, friction=0.00
     for row_ind, row in enumerate(arr):
         for col_ind, (prev_th, prev_om) in enumerate(row):
             omega = (
-                    prev_om * (1 - friction)
+                    prev_om * (1 - friction * step_size)
                     - GRAV / rope * math.sin(prev_th) * step_size
             )
             theta = prev_th + prev_om * step_size
@@ -185,13 +186,12 @@ def pendulum_gradient(plot_range=5, N=45, rope=3, step_size=0.001, friction=0.00
 "Model Creation"
 
 
-def run_simulation_pendulum(gradient_ax=None, friction=0.0, time_range=1000, step_size=0.01,
-                      initial_theta=0.0, initial_omega=0.0):
+def run_simulation_pendulum(name=None, gradient_ax=None, friction=0.0, time_range=1000, step_size=0.01,
+                            initial_theta=0.0, initial_omega=0.0):
     center = (0, 0)
     model = SinglePendulum(*center, mass=1, rope=3, initial_theta=initial_theta, initial_omega=initial_omega,
                            step_size=step_size,
                            friction=friction)
-    # initial = model.x, model.y
     all_trace = []
     offset_x = 300
     offset_y = 300
@@ -220,156 +220,75 @@ def run_simulation_pendulum(gradient_ax=None, friction=0.0, time_range=1000, ste
 
             if SHOW:
                 cv2.imshow("Pendulum", array)
-                key = cv2.waitKey(1)
+                key = cv2.waitKey(5)
                 if key == ord("q"):
                     break
 
-    trace = list([*zip(*all_trace)])
+    if PLOT:
+        fig1 = plt.figure()
+        plt.title("Pendulum energy state")
+        plt.plot(model.data['total_en'], label="total en")
+        plt.plot(model.data['ek'], label="kinetic")
+        plt.plot(model.data['ep'], label="potential")
+        ticks = plt.xticks()[0]
+        plt.xticks(np.linspace(ticks[0], ticks[-1], 15))
+        plt.legend(loc=1)
 
-    fig1 = plt.figure()
-    plt.title("Pendulum energy state")
-    plt.plot(model.data['total_en'], label="total en")
-    plt.plot(model.data['ek'], label="kinetic")
-    plt.plot(model.data['ep'], label="potential")
-    ticks = plt.xticks()[0]
-    plt.xticks(np.linspace(ticks[0], ticks[-1], 15))
-    plt.legend(loc=1)
+        fig2 = plt.figure()
+        ax1 = plt.axes([0.1, 0.05, 0.8, 0.6])
+        ax2 = plt.axes([0.1, 0.7, 0.8, 0.2])
 
-    fig2 = plt.figure()
-    ax1 = plt.axes([0.1, 0.05, 0.8, 0.6])
-    ax2 = plt.axes([0.1, 0.7, 0.8, 0.2])
+        ax1.plot(model.data['lin_ve'], label="lin_ve", c=(1, 0, 0.7), linewidth=1)
+        ax1.plot(model.data['omega'], label="omega", linewidth=1)
+        plt.title("Position and speed")
+        ax1.legend()
 
-    # ax1.plot(model.data['veloc'], label="velocity (from mgh)", c='r')
-    ax1.plot(model.data['lin_ve'], label="lin_ve", c=(1, 0, 0.7), linewidth=1)
-    # ax1.plot(model.data['lin_ve2'], label="lin_ve2", c=(0.5, 0.5, 1), dashes=[10, 5, 5, 5], linewidth=3)
-    ax1.plot(model.data['omega'], label="omega", linewidth=1)
-    # ax1.plot(model.data['radial'], label="radial")
-    # ax1.plot(model.data['radial_deg'], label="radial_deg")
-    # ax1.plot(model.data['factor'], label="factor")
+        ax2.plot(model.data['theta_deg'], label="theta degrees")
+        ax2.legend()
 
-    # ax1.set_xticks(np.arange(0, 250, 10.0))
-    # ax2.set_xticks(np.arange(0, 250, 10.0))
-    plt.title("Position and speed")
-    ax1.legend()
+        plt.figure()
+        plt.plot(model.data['x'], label="x", )
+        plt.plot(model.data['y'], label='y', c=[1, 0.2, .6])
+        plt.plot(model.data['h'], label='h', dashes=[4, 4, 2, 4], c=[0.8, 0.2, .2])
 
-    ax2.plot(model.data['theta_deg'], label="theta degrees")
-    ax2.legend()
-
-    plt.figure()
-    # plt.subplot(312)
-    plt.plot(model.data['x'], label="x", )
-    plt.plot(model.data['y'], label='y', c=[1, 0.2, .6])
-    plt.plot(model.data['h'], label='h', dashes=[4, 4, 2, 4], c=[1, 0.2, .6])
-
-    ticks = plt.xticks()[0]
-    plt.xticks(np.linspace(ticks[0], ticks[-1], 15))
-    plt.legend()
+        ticks = plt.xticks()[0]
+        plt.xticks(np.linspace(ticks[0], ticks[-1], 15))
+        plt.legend()
 
     if gradient_ax:
+        if not name:
+            name = "Pendulum"
         # omg = np.array(model.data['omega']) * -1
         omg = np.array(model.data['omega'])
-        gradient_ax.plot(model.data['theta'], omg, label="Pendulum", color=(0, 0.7, 0.9))
+        c = np.random.random(3)
+        c[1] = c[1] * 0.4 + 0.5
+        gradient_ax.plot(model.data['theta'], omg, label=name, color=c)
         gradient_ax.legend(loc=1)
         fig = gradient_ax.figure
         fig.savefig("Pendulum path.jpg")
 
 
-def run_vec_simulation_pendulum(gradient_ax=None, friction=0.0, time_range=1000, step_size=0.01,
-                                initial_theta=0.0, initial_omega=0.0):
-    center = (0, 0)
-    model = VectorPendulum(*center, mass=1, rope=3, initial_theta=initial_theta, initial_omega=initial_omega,
-                           step_size=step_size,
-                           friction=friction)
-    # initial = model.x, model.y
-    all_trace = []
-    offset_x = 300
-    offset_y = 300
-
-    for ti in range(time_range):
-        model.step()
-        x, y = model.data['x'][-1], model.data['y'][-1]
-        all_trace.append((x, y))
-        if RENDER or SHOW:
-            array = np.zeros((600, 600, 3), dtype=np.uint8) + 170
-            poly = np.array(all_trace[-30:])
-            poly[:, 1] *= -1
-            poly[:, :] *= 50
-            poly[:, :] += 300
-            x = int(x * 50) + 300
-            y = int(-y * 50) + 300
-
-            cv2.line(array, (offset_x, offset_y), (x, y), (0, 0, 0), 2)  # Line
-            cv2.circle(array, (offset_x, offset_y), 10, (0, 0, 160), -1)  # Anchor
-            for n, tr in enumerate(poly):
-                tr = np.array(tr, dtype=np.int32)
-                col = np.array([50, 200, 0]) * (n / 30)
-                cv2.circle(array, tuple(tr), 8, col, -1)
-            cv2.circle(array, (x, y), 10, (250, 100, 50), -1)  # Ball
-            cv2.putText(array, f"{ti:,}", (50, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), )
-
-            if SHOW:
-                cv2.imshow("Pendulum", array)
-                key = cv2.waitKey(1)
-                if key == ord("q"):
-                    break
-
-    trace = list([*zip(*all_trace)])
-
-    fig1 = plt.figure()
-    plt.title("Pendulum energy state")
-    plt.plot(model.data['total_en'], label="total en")
-    plt.plot(model.data['ek'], label="kinetic")
-    plt.plot(model.data['ep'], label="potential")
-    ticks = plt.xticks()[0]
-    plt.xticks(np.linspace(ticks[0], ticks[-1], 15))
-    plt.legend(loc=1)
-
-    fig2 = plt.figure()
-    ax1 = plt.axes([0.1, 0.05, 0.8, 0.6])
-    ax2 = plt.axes([0.1, 0.7, 0.8, 0.2])
-
-    # ax1.plot(model.data['veloc'], label="velocity (from mgh)", c='r')
-    ax1.plot(model.data['lin_ve'], label="lin_ve", c=(1, 0, 0.7), linewidth=1)
-    # ax1.plot(model.data['lin_ve2'], label="lin_ve2", c=(0.5, 0.5, 1), dashes=[10, 5, 5, 5], linewidth=3)
-    ax1.plot(model.data['omega'], label="omega", linewidth=1)
-    # ax1.plot(model.data['radial'], label="radial")
-    # ax1.plot(model.data['radial_deg'], label="radial_deg")
-    # ax1.plot(model.data['factor'], label="factor")
-
-    # ax1.set_xticks(np.arange(0, 250, 10.0))
-    # ax2.set_xticks(np.arange(0, 250, 10.0))
-    plt.title("Position and speed")
-    ax1.legend()
-
-    ax2.plot(model.data['theta_deg'], label="theta degrees")
-    ax2.legend()
-
-    plt.figure()
-    # plt.subplot(312)
-    plt.plot(model.data['x'], label="x", )
-    plt.plot(model.data['y'], label='y', c=[1, 0.2, .6])
-    plt.plot(model.data['h'], label='h', dashes=[4, 4, 2, 4], c=[1, 0.2, .6])
-
-    ticks = plt.xticks()[0]
-    plt.xticks(np.linspace(ticks[0], ticks[-1], 15))
-    plt.legend()
-
-    if gradient_ax:
-        # omg = np.array(model.data['omega']) * -1
-        omg = np.array(model.data['omega'])
-        gradient_ax.plot(model.data['theta'], omg, label="Pendulum", color=(0, 0.7, 0.9))
-        gradient_ax.legend(loc=1)
-        fig = gradient_ax.figure
-        fig.savefig("Pendulum path.jpg")
-
-
-friction = 0.005
+friction = 0.4
+# friction = 0
 step_size = 0.01
 SHOW = False
 RENDER = False
+PLOT = False
 
-ax = pendulum_gradient(plot_range=10, friction=friction, step_size=step_size)
-run_simulation_pendulum(ax, friction=friction, time_range=1_500, step_size=step_size,
-                  initial_theta=math.radians(180 * 3), initial_omega=-8.2)
+# ax = None
+initial_theta = math.radians(179 + 180)
+initial_omega = -7.2 + 0.4
+time_range = 5_000
+ax = pendulum_gradient(N=70, plot_range=10, friction=friction, step_size=step_size)
+
+run_simulation_pendulum(f"Pendulum, {step_size}", ax,
+                        friction=friction, time_range=time_range, step_size=step_size,
+                        initial_theta=initial_theta, initial_omega=initial_omega)
+run_simulation_pendulum(f"Pendulum2 {step_size * 10}", ax,
+                        friction=friction, time_range=time_range // 10, step_size=step_size * 10,
+                        initial_theta=initial_theta, initial_omega=initial_omega)
+run_simulation_pendulum(f"Pendulum3 {step_size / 10}", ax,
+                        friction=friction, time_range=time_range * 10, step_size=step_size / 10,
+                        initial_theta=initial_theta, initial_omega=initial_omega)
 
 plt.show()
